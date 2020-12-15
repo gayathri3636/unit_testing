0bd420c7082adf5626828a22b14495853572e55a
Gayathri Nannapaneni <gayathri.pegasian@gmail.com>
2020-12-15 15:59:30 -0500

    Checking with proptypes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React, { Component } from 'react'
 import './App.scss';
+import Header from './components/Header/index';
+import Headline from './components/headline/index';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const tempArr = [{
+  fName:"Joe",
+  lName:"Biden",
+  email:'joebiden@gmail.com',
+  age:68,
+  onlineStatus:true
+}]
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+    <section className="headline">
+    <Headline header="Posts" desc="click the button to render posts" tempArr={tempArr}/>
+    </section>
+      </div>
+    )
+  }
 }
 
-export default App;
